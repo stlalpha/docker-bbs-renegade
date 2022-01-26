7e906c80ae467eb8c622dd6b57fb25781c7f9e30
stlalpha <stlalpha@icloud.com>
2022-01-25 21:58:40 -0600

    sorted sums

--- nodeprephook.sh
+++ nodeprephook.sh
@@ -2,10 +2,10 @@
 if [ ! -e $STARTUP ]; then
   echo "H:" >> $STARTUP.tmp
   echo "cd ADF" >> $STARTUP.tmp
-  echo "lh adf COM1 3F8  4 115200 8192  8192  8" >> $STARTUP.tmp
+  echo "lh adf COM1 3F8  4 57600 8192  8192  8" >> $STARTUP.tmp
   echo "G:" >> $STARTUP.tmp
-  echo "cd rg" >> $STARTUP.tmp
-  echo "renegade /N$FOUNDNODE -Q -B115200" >> $STARTUP.tmp
+  echo "cd v2/NODE$FOUNDNODE" >> $STARTUP.tmp
+  echo "ViSiON /N$FOUNDNODE -Q -B57600" >> $STARTUP.tmp
   echo "exitemu" >> $STARTUP.tmp
   unix2dos $STARTUP.tmp &> /dev/null
   mv $STARTUP.tmp $STARTUP
--- setup/setup.sh
+++ setup/setup.sh
@@ -5,29 +5,23 @@ set -x
 
 # Download
 cd /tmp/setup
-# renegadebbs.info wend down in July 2017.  Use the archive.org copy.
-# wget http://renegadebbs.info/files/unofficial.rgbbsfullv120a.zip
-#wget 'https://web.archive.org/web/20160324195446/http://renegadebbs.info/files/unofficial.rgbbsfullv120a.zip'
+
+mkdir /dos/drive_g/v2
+wget https://github.com/stlalpha/docker-bbs-renegade/blob/master/binaries/ViSiON2-RES.ZIP?raw=true
 
 sha256sum -c < sums
 
-cd /dos/drive_g
-unzip /tmp/setup/unofficial.rgbbsfullv120a.zip
+cd /dos/drive_g/v2
+
+unzip /tmp/setup/ViSiON2-RES.ZIP
 rm /tmp/setup/unofficial.rgbbsfullv120a.zip
-cd rg/DATA
 
-# set up all 9 nodes.
-for ASDF in 2 3 4 5 6 7 8 9; do
-  cp NODE1.DAT NODE$ASDF.DAT
-done
-mv /tmp/setup/MULTNODE.DAT .
+cd v2
 
-cd /dos/drive_g/rg
+mkdir NODE1
+mkdir NODE2
 
-# Patch the paths
-find . -type f -exec sed -i 's,C:\\RG,G:\\RG,g' {} \;
 
-cd /dos
 
 echo 2 > /dos/numnodes   # 2 nodes by default
 
