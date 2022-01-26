ee7a995311a33e1fb517b65adcf9b92da3d3c5a3
stlalpha <stlalpha@icloud.com>
2022-01-25 22:58:01 -0600

    dszlog is important

--- nodeprephook.sh
+++ nodeprephook.sh
@@ -4,8 +4,9 @@ if [ ! -e $STARTUP ]; then
   echo "cd ADF" >> $STARTUP.tmp
   echo "lh adf COM1 3F8  4 57600 8192  8192  8" >> $STARTUP.tmp
   echo "G:" >> $STARTUP.tmp
-  echo "cd v2/NODE$FOUNDNODE" >> $STARTUP.tmp
-  echo "ViSiON /N$FOUNDNODE /57600" >> $STARTUP.tmp
+  echo "SET DSZLOG=G:\V2\XFER"
+  echo "cd v2" >> $STARTUP.tmp
+  echo "ViSiON /57600" >> $STARTUP.tmp
   echo "exitemu" >> $STARTUP.tmp
   unix2dos $STARTUP.tmp &> /dev/null
   mv $STARTUP.tmp $STARTUP
