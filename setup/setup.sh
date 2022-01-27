d6948304be3187f6b20929178bb0578577eb335e
Grady McBride <grady@stalpgo.com>
2022-01-27 09:26:04 -0600

    fixed zip path

--- setup/setup.sh
+++ setup/setup.sh
@@ -11,11 +11,11 @@ wget -O ViSiON2-RES.ZIP https://github.com/stlalpha/docker-bbs-renegade/blob/mas
 
 sha256sum -c < sums
 
-cd /dos/drive_g/v2
+cd /dos/drive_g
 
 unzip /tmp/setup/ViSiON2-RES.ZIP
 rm /tmp/setup/ViSiON2-RES.ZIP
-cd /dos/drive_g/v2
+#cd /dos/drive_g/v2
 #unzip TOS_MENU.ZIP -d ./MENUS
 #unzip MENUDATA.ZIP -d ./MENUDATA
 
