27342034ed988f25cb60c2fed5299ec1b8ad3102
Grady McBride <grady@stalpgo.com>
2022-01-30 21:56:00 -0600

    v2r anyone?

--- setup/setup.sh
+++ setup/setup.sh
@@ -6,12 +6,12 @@ set -x
 # Download
 cd /tmp/setup
 
-mkdir /dos/drive_g/v2
+mkdir /dos/drive_g/v2r
 wget -O ViSiON2-RES.ZIP https://github.com/stlalpha/docker-bbs-renegade/blob/master/binaries/V2R0043D.ZIP?raw=true
 
 sha256sum -c < sums
-cp /tmp/setup/ViSiON2-RES.ZIP /dos/drive_g/v2
-cd /dos/drive_g/v2
+#cp /tmp/setup/ViSiON2-RES.ZIP /dos/drive_g/v2
+cd /dos/drive_g/v2r
 unzip /tmp/setup/ViSiON2-RES.ZIP
 rm /tmp/setup/ViSiON2-RES.ZIP
 #cd /dos/drive_g/v2
