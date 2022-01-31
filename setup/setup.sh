3611a05e063c5a9dc26a9438bcca10b50f4c974c
Grady McBride <grady@stalpgo.com>
2022-01-30 21:59:26 -0600

    fewer commands == better

--- setup/setup.sh
+++ setup/setup.sh
@@ -6,14 +6,13 @@ set -x
 # Download
 cd /tmp/setup
 
-mkdir /dos/drive_g/v2r
-wget -O ViSiON2-RES.ZIP https://github.com/stlalpha/docker-bbs-renegade/blob/master/binaries/V2R0043D.ZIP?raw=true
+mkdir /dos/drive_g/v2r && cd /dos/drive_g/v2r
+wget -O V2RES.ZIP https://github.com/stlalpha/docker-bbs-renegade/blob/master/binaries/V2R0043D.ZIP?raw=true
 
 sha256sum -c < sums
 #cp /tmp/setup/ViSiON2-RES.ZIP /dos/drive_g/v2
-cd /dos/drive_g/v2r
-unzip /tmp/setup/ViSiON2-RES.ZIP
-rm /tmp/setup/ViSiON2-RES.ZIP
+unzip V2RES.ZIP
+rm V2RES.ZIP
 #cd /dos/drive_g/v2
 #unzip TOS_MENU.ZIP -d ./MENUS
 #unzip MENUDATA.ZIP -d ./MENUDATA
