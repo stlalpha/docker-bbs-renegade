f36e4d5ae924697b3bde79a23b575be2f57f32e4
Grady McBride <grady@stalpgo.com>
2022-01-30 22:13:42 -0600

    setup.sh directory change too early

--- setup/setup.sh
+++ setup/setup.sh
@@ -6,10 +6,11 @@ set -x
 # Download
 cd /tmp/setup
 
-mkdir /dos/drive_g/v2r && cd /dos/drive_g/v2r
+mkdir /dos/drive_g/v2r 
 wget -O V2RES.ZIP https://github.com/stlalpha/docker-bbs-renegade/blob/master/binaries/V2R0043D.ZIP?raw=true
 
 sha256sum -c < sums
+mv V2RES.ZIP /dos/drive_g/v2r && cd /dos/drive_g/v2r
 #cp /tmp/setup/ViSiON2-RES.ZIP /dos/drive_g/v2
 unzip V2RES.ZIP
 rm V2RES.ZIP
