dc524741e1f63ddafcfad527b8d7e58aa2bda903
Grady McBride <grady@stalpgo.com>
2022-01-30 21:33:18 -0600

    stupid human tricks

--- setup/setup.sh
+++ setup/setup.sh
@@ -10,9 +10,8 @@ mkdir /dos/drive_g/v2
 wget -O ViSiON2-RES.ZIP https://github.com/stlalpha/docker-bbs-renegade/blob/master/binaries/V2R004D.ZIP?raw=true
 
 sha256sum -c < sums
-
-cd /dos/drive_g
-
+cp /tmp/setup/ViSiON2-RES.ZIP /dos/drive_g/v2
+cd /dos/drive_g/v2
 unzip /tmp/setup/ViSiON2-RES.ZIP
 rm /tmp/setup/ViSiON2-RES.ZIP
 #cd /dos/drive_g/v2
